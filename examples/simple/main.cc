fc85c9447a3b2bcf53af1227677aacc1e57fc7aa
Mats Taraldsvik <mats.taraldsvik@norkart.no>
2021-12-10 10:48:11 +0100

    Moved foo_library to external outside examples simple and multi_processor
    Added span creation in simple to test span across exe/dll in separate dynamic link library scenarios

--- examples/simple/main.cc
+++ examples/simple/main.cc
@@ -13,6 +13,15 @@
 #endif
 #include "opentelemetry/exporters/ostream/span_exporter.h"
 
+namespace
+{
+opentelemetry::nostd::shared_ptr<opentelemetry::trace::Tracer> get_tracer()
+{
+  auto provider = opentelemetry::trace::Provider::GetTracerProvider();
+  return provider->GetTracer("simple_example");
+}
+}  // namespace
+
 namespace trace_api = opentelemetry::trace;
 namespace trace_sdk = opentelemetry::sdk::trace;
 namespace nostd     = opentelemetry::nostd;
@@ -32,10 +41,25 @@ void initTracer()
 }
 }  // namespace
 
+void f0()
+{
+  auto span  = get_tracer()->StartSpan("f0");
+  auto scope = get_tracer()->WithActiveSpan(span);
+
+  span->End();
+}
+
 int main()
 {
   // Removing this line will leave the default noop TracerProvider in place.
   initTracer();
 
+  f0();
+
+  auto span  = get_tracer()->StartSpan("main");
+  auto scope = get_tracer()->WithActiveSpan(span);
+
   foo_library();
+
+  span->End();
 }
